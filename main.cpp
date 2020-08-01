f909e0f2e1c0f59f307e22af90d9ef900119b793
SeungHun Han <shhan730@gmail.com>
2020-08-02 00:29:39 +0900

    Inital Commit

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include <cstdio>
+#include <pcap.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "ethhdr.h"
+#include "arphdr.h"
+
+
+
+#pragma pack(push, 1)
+struct EthArpPacket {
+	EthHdr eth_;
+	ArpHdr arp_;
+};
+#pragma pack(pop)
+
+void usage() {
+    printf("syntax : send-arp <interface> <sender ip> <target ip>\n");
+    printf("sample : send-arp wlan0 192.168.10.2 192.168.10.1\n");
+}
+
+void get_mac_addr(unsigned char MAC_str[13])
+{
+    #define HWADDR_len 6
+    int s,i;
+    struct ifreq ifr;
+    s = socket(AF_INET, SOCK_DGRAM, 0);
+    strcpy(ifr.ifr_name, "br-wlan");
+    ioctl(s, SIOCGIFHWADDR, &ifr);
+    for (i=0; i<HWADDR_len; i++)
+        sprintf(&MAC_str[i*2],"%02X",((unsigned char*)ifr.ifr_hwaddr.sa_data)[i]);
+    MAC_str[12]='\0';
+}
+
+char* get_sender_mac(char* sender_ip){
+    EthArpPacket packet;
+
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+		usage();
+		return -1;
+	}
+
+	char* dev = argv[1];
+	char errbuf[PCAP_ERRBUF_SIZE];
+	pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+	if (handle == nullptr) {
+		fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
+		return -1;
+	}
+
+	EthArpPacket packet;
+
+	packet.eth_.dmac_ = Mac("00:00:00:00:00:00");
+	packet.eth_.smac_ = Mac("00:00:00:00:00:00");
+	packet.eth_.type_ = htons(EthHdr::Arp);
+
+	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
+	packet.arp_.pro_ = htons(EthHdr::Ip4);
+	packet.arp_.hln_ = Mac::SIZE;
+	packet.arp_.pln_ = Ip::SIZE;
+	packet.arp_.op_ = htons(ArpHdr::Request);
+	packet.arp_.smac_ = Mac("00:00:00:00:00:00");
+	packet.arp_.sip_ = htonl(Ip("0.0.0.0"));
+	packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
+	packet.arp_.tip_ = htonl(Ip("0.0.0.0"));
+
+	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
+	if (res != 0) {
+		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+	}
+
+	pcap_close(handle);
+}
